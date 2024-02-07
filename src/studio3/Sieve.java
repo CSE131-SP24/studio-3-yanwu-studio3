adee6ee2a6c10c07dda19f2299d39dbba69257ba
enbunka <enbunka@jana-yans-mbp.dhcp.wustl.edu>
2024-02-07 14:24:31 -0600

    1

--- src/studio3/Sieve.java
+++ src/studio3/Sieve.java
@@ -1,18 +1,32 @@
 package studio3;
 
+import java.util.Scanner;
+
 public class Sieve {
 
 	public static void main(String[] args) {
-		int integer = 1;
 		
-		while (integer <=100)
-		{
-			System.out.println("integer" + integer);
-	    	integer = integer +1;
-	    	int[] number = {integer};
+		Scanner in = new Scanner(System.in);
+		System.out.println("calcupate prime till");
+		int n= in.nextInt();
+		
+		//int[] p = new int [n]; 
+		boolean[] eliminated = new boolean [n];
+	
+			for (int currMult=2; currMult <= Math.sqrt(n); currMult++)
+				{
+				//if (i%p >0) is prime number
+				//else if not prime number
+					
+					
+				
+			}
+	
 			
-		}
+		
+	
 
 	}
-
-}
+}	
+		
+		
