3d131c02c6d4d2612c38dccb540c00ddb32d96f5
enbunka <enbunka@jana-yans-mbp.dhcp.wustl.edu>
2024-02-07 14:24:26 -0600

    122

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,18 @@
+package studio3;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		int integer = 1;
+		
+		while (integer <=100)
+		{
+			System.out.println("integer" + integer);
+	    	integer = integer +1;
+	    	int[] number = {integer};
+			
+		}
+
+	}
+
+}
